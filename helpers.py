f8051f73be5e1d50f7ac67a6675db369e0424327
Xiao Wang <davidandthings@hey.com>
2021-11-18 15:07:08 -0500

    read table with specified names

--- helpers.py
+++ helpers.py
@@ -137,7 +137,7 @@ def interpolate_map(query_addr, gene_map_addr, output_addr):
     #gen_data = np.loadtxt(gene_map_addr, dtype={
     #    'names': ['RSID', 'position', 'gen_dist'], 'formats': ['S20', 'i8', 'f8']})
 
-    gen_data = pd.read_table(gene_map_addr, dtype={'names': ['RSID', 'position', 'gen_dist']})
+    gen_data = pd.read_table(gene_map_addr, names=['RSID', 'position', 'gen_dist'])
 
     gen_dict = {}
 
