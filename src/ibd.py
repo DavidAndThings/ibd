d27dbf41aa6dd350554cfcf56065fa1dbe9a5c60
davidandthings@hey.com <donkeyzrage@pop-os.localdomain>
2022-01-27 15:45:01 -0500

    added toilash handler

--- src/ibd.py
+++ src/ibd.py
@@ -14,6 +14,7 @@ if __name__ == '__main__':
     graph_compiler = GraphHandler()
     infomap_handler = InfoMapHandler()
     shapeit_handler = ShapeItHandler()
+    toilash_handler = ToIlashHandler()
 
     sample_conv.set_next(haps_conv)
     haps_conv.set_next(ilash_run)
@@ -22,6 +23,7 @@ if __name__ == '__main__':
     qc_run.set_next(graph_compiler)
     graph_compiler.set_next(infomap_handler)
     infomap_handler.set_next(shapeit_handler)
+    shapeit_handler.set_next(toilash_handler)
 
     args = p.parse_args()
     sample_conv.handle(args)
