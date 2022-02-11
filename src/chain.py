99f25ae5f83d52f2be221cc42a721cfdd16c83f8
davidandthings@hey.com <donkeyzrage@pop-os.localdomain>
2022-02-11 16:05:21 -0500

    now using the functional decorator pattern to handle user inputs

--- src/chain.py
+++ src/chain.py
@@ -1,4 +1,3 @@
-from abc import ABC, abstractmethod
 from helpers import sample_to_fam, haps_to_ped, run_ilash, build_map_file
 from qc import get_high_quality_regions, plot_hits
 from graph import FileSampleGraph, build_graph_from_file, filter_sample_graph
@@ -12,148 +11,44 @@ from phased_toilash import deploy_phased_toilash
 from to_infomap import ToInfomap
 
 
-class CommandHandler(ABC):
+def to_infomap_function(handler):
 
-    def __init__(self):
-        self.__next = None
-
-    @abstractmethod
-    def handle(self, request):
-        pass
-
-    def has_next(self):
-        return self.__next is not None
-
-    def set_next(self, next_handler):
-        self.__next = next_handler
-
-    def get_next(self):
-        return self.__next
-
-
-class SampleConvertHandler(CommandHandler):
-
-    def handle(self, request):
-
-        if request.tool == "convert" and request.type == "sample":
-            sample_to_fam(request.sample, request.output)
-
-        elif self.has_next():
-            self.get_next().handle(request)
-
-
-class HapsConvertHandler(CommandHandler):
-
-    def handle(self, request):
-
-        if request.tool == "convert" and request.type == "haps":
-            haps_to_ped(request.haps, request.fam, request.output)
-
-        elif self.has_next():
-            self.get_next().handle(request)
-
-
-class DistConvertHandler(CommandHandler):
-
-    def handle(self, request):
-
-        if request.tool == "convert" and request.type == "dist":
-            build_map_file(request.haps, request.dist, request.chr, request.output)
-
-        elif self.has_next():
-            self.get_next().handle(request)
-
-
-class ILASHHandler(CommandHandler):
-
-    def handle(self, request):
-
-        if request.tool == "ilash":
-            run_ilash(request.ped, request.map, request.output)
-
-        elif self.has_next():
-            self.get_next().handle(request)
-
-
-class QcHandler(CommandHandler):
-
-    def handle(self, request):
-
-        if request.tool == "qc":
-
-            output_match = request.output + ".match"
-            output_figure = request.output + ".png"
-
-            get_high_quality_regions(
-                request.map, request.match, request.chromosome, 
-                output_match, request.identified
-            )
-
-            plot_hits(
-                request.map, request.match, request.chromosome, 
-                output_match, output_figure
-            )
-
-        elif self.has_next():
-            self.get_next().handle(request)
-
-
-class GraphHandler(CommandHandler):
-
-    def handle(self, request):
+    def to_infomap_handler(request):
         
-        if request.tool == "graph" and request.graph_tools == "build":
-            
-            onlyfiles = [join(request.match_dir, f) for f in listdir(request.match_dir) if isfile(join(request.match_dir, f)) and f.endswith(".match")]
-            sample_graph = FileSampleGraph(31, 97)
-            sample_graph.build_storage()
-
-            for f in onlyfiles:
-                build_graph_from_file(f, sample_graph)
-            
-            sample_graph.flush_adjacency_list(request.output)
-            sample_graph.purge()
-        
-        elif request.tool == "graph" and request.graph_tools == "filter":
-
-            filter_sample_graph(request.graph, request.exclude, request.output)
-
-        elif self.has_next():
-            self.get_next().handle(request)
-
-
-class InfoMapHandler(CommandHandler):
-
-    def handle(self, request):
-        
-        if request.tool == "infomap":
-
-            run_infomap(request.graph, request.output_prefix)
-
-        elif self.has_next():
-            self.get_next().handle(request)
-
-
-class ShapeItHandler(CommandHandler):
-
-    def handle(self, request):
-
-        if request.tool == "shapeit":
+        if request.tool == "to_infomap":
 
             with open(request.config) as config_file:
 
                 config_data = json.load(config_file)
-                shapeit_workflow = ShapeIt(config_data)
-                shapeit_workflow.run()
+                job = ToInfomap(config_data["to_infomap"])
+                job.run()
         
-        elif self.has_next():
-            
-            self.get_next().handle(request)
+        else:
+            handler(request)
+
+    return to_infomap_handler
 
 
-class ToIlashHandler(CommandHandler):
+def phased_toilash_function(handler):
 
-    def handle(self, request):
+    def phased_toilash_handler(request):
+        
+        if request.tool == "phased_toilash":
+
+            with open(request.config) as config_file:
+                
+                config_data = json.load(config_file)
+                deploy_phased_toilash(config_data["phased_toilash"])
+        
+        else:
+            handler(request)
+
+    return phased_toilash_handler
+
+
+def toilash_function(handler):
+
+    def toilash_handler(request):
 
         if request.tool == "toilash":
 
@@ -170,40 +65,156 @@ class ToIlashHandler(CommandHandler):
                     
                     deploy_to_ilash(config_data["toilash_multiple"])
         
-        elif self.has_next():
+        else:
+            handler(request)
 
-            self.get_next().handle(request)
-
-
-class PhasedToIlashHandler(CommandHandler):
-
-    def handle(self, request):
-
-        if request.tool == "phased_toilash":
-
-            with open(request.config) as config_file:
-                
-                config_data = json.load(config_file)
-                deploy_phased_toilash(config_data["phased_toilash"])
-        
-        elif self.has_next():
-
-            self.get_next().handle(request)
+    return toilash_handler
 
 
-class ToInfomapHandler(CommandHandler):
+def shapeit_function(handler):
 
-    def handle(self, request):
+    def shapeit_handler(request):
 
-        if request.tool == "to_infomap":
+        if request.tool == "shapeit":
 
             with open(request.config) as config_file:
 
                 config_data = json.load(config_file)
-                job = ToInfomap(config_data["to_infomap"])
-                job.run()
+                shapeit_workflow = ShapeIt(config_data)
+                shapeit_workflow.run()
         
-        elif self.has_next():
+        else:
+            handler(request)
 
-            self.get_next().handle(request)
+    return shapeit_handler
 
+
+def infomap_function(handler):
+
+    def infomap_handler(request):
+
+        if request.tool == "infomap":
+            run_infomap(request.graph, request.output_prefix)
+        
+        else:
+            handler(request)
+
+    return infomap_handler
+
+
+def graph_function(handler):
+
+    def graph_handler(request):
+        
+        if request.tool == "graph" and request.graph_tools == "build":
+            
+            onlyfiles = [join(request.match_dir, f) for f in listdir(request.match_dir) if isfile(join(request.match_dir, f)) and f.endswith(".match")]
+            sample_graph = FileSampleGraph(31, 97)
+            sample_graph.build_storage()
+
+            for f in onlyfiles:
+                build_graph_from_file(f, sample_graph)
+            
+            sample_graph.flush_adjacency_list(request.output)
+            sample_graph.purge()
+        
+        elif request.tool == "graph" and request.graph_tools == "filter":
+
+            filter_sample_graph(request.graph, request.exclude, request.output)
+        
+        else:
+            handler(request)
+
+    return graph_handler
+
+
+def qc_function(handler):
+
+    def qc_handler(request):
+
+        if request.tool == "qc":
+
+            output_match = request.output + ".match"
+            output_figure = request.output + ".png"
+
+            get_high_quality_regions(
+                request.map, request.match, request.chromosome, 
+                output_match, request.identified
+            )
+
+            plot_hits(
+                request.map, request.match, request.chromosome, 
+                output_match, output_figure
+            )
+        
+        else:
+            handler(request)
+
+    return qc_handler
+
+
+def ilash_function(handler):
+
+    def ilash_handler(request):
+        
+        if request.tool == "ilash":
+            run_ilash(request.ped, request.map, request.output)
+        
+        else:
+            handler(request)
+
+    return ilash_handler
+
+
+def dist_convert_function(handler):
+
+    def dist_convert_handler(request):
+
+        if request.tool == "convert" and request.type == "dist":
+            build_map_file(request.haps, request.dist, request.chr, request.output)
+        
+        else:
+            handler(request)
+
+    return dist_convert_handler
+
+
+def haps_convert_function(handler):
+
+    def haps_convert_handler(request):
+
+        if request.tool == "convert" and request.type == "haps":
+            haps_to_ped(request.haps, request.fam, request.output)
+        
+        else:
+            handler(request)
+    
+    return haps_convert_handler
+
+
+def sample_convert_function(handler):
+
+    def sample_convert_handler(request):
+        
+        if request.tool == "convert" and request.type == "sample":
+            sample_to_fam(request.sample, request.output)
+        
+        else:
+            handler(request)
+
+    return sample_convert_handler
+
+
+@to_infomap_function
+@phased_toilash_function
+@toilash_function
+@shapeit_function
+@infomap_function
+@graph_function
+@qc_function
+@ilash_function
+@dist_convert_function
+@haps_convert_function
+@sample_convert_function
+def master_handler(request):
+    print("Input is invalid :(")
--- src/ibd.py
+++ src/ibd.py
@@ -1,33 +1,9 @@
 from parser import build_parser
-from chain import *
+from chain import master_handler
 
 
 if __name__ == '__main__':
 
     p = build_parser()
-
-    sample_conv = SampleConvertHandler()
-    haps_conv = HapsConvertHandler()
-    ilash_run = ILASHHandler()
-    dist_conv = DistConvertHandler()
-    qc_run = QcHandler()
-    graph_compiler = GraphHandler()
-    infomap_handler = InfoMapHandler()
-    shapeit_handler = ShapeItHandler()
-    toilash_handler = ToIlashHandler()
-    phased_toilash_handler = PhasedToIlashHandler()
-    to_infomap_handler = ToInfomapHandler()
-
-    sample_conv.set_next(haps_conv)
-    haps_conv.set_next(ilash_run)
-    ilash_run.set_next(dist_conv)
-    dist_conv.set_next(qc_run)
-    qc_run.set_next(graph_compiler)
-    graph_compiler.set_next(infomap_handler)
-    infomap_handler.set_next(shapeit_handler)
-    shapeit_handler.set_next(toilash_handler)
-    toilash_handler.set_next(phased_toilash_handler)
-    phased_toilash_handler.set_next(to_infomap_handler)
-
     args = p.parse_args()
-    sample_conv.handle(args)
+    master_handler(args)
